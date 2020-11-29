d7be0636c074969d1a7f7a485342c62646957197
ckonig <itckoenig@gmail.com>
2020-11-29 01:13:13 +0100

    component cleanup

--- src/Game/PlayStack.js
+++ src/Game/PlayStack.js
@@ -9,14 +9,12 @@ export default class PlayStack extends Base {
         if (this.hand.isHoldingCard() && !this.hand.isCurrentCard(card)) {
             if (this.state().hand.source == 'main' || this.state().hand.source == 'play') {
                 this.stateHolder.setState((state, props) => {
-                    var current = state.currentCard; //@todo use hand stack card
+                    var current = this.hand.currentCard();
                     var top = this.state().playStack[this.state().playStack.length - 1];
-                    if (current && !top || top.face !== current.props.face || top.type.icon !== current.props.type.icon) {
+                    if (current && current.props && (!top || top.face !== current.props.face || top.type.icon !== current.props.type.icon)) {
                         state.playStack.push(this.hand.currentCard().props);
                     }
-                    state.hand.stack = []
-                    state.hand.source = null;
-                    return { ...state, currentCard: null };
+                    return { ...this.unselectCard(state) };
                 });
             }
         } else if (card && !this.hand.isHoldingCard()) {
--- src/Game/TargetStack.js
+++ src/Game/TargetStack.js
@@ -1,39 +1,31 @@
 import Base from "./Base";
 
 export default class TargetStack extends Base {
+
     constructor(stateholder) {
         super(stateholder)
     }
-    click = (index) => {
-        if (this.hand.isHoldingCard()) {
-            if (this.state().targetStacks[index].icon == this.hand.currentCard().props.type.icon) {
-                var currentAccepted = this.state().targetStacks[index].acceptedCards[this.state().targetStacks[index].acceptedCards.length - 1];
-                if (currentAccepted == this.hand.currentCard().props.face) {
-                    this.stateHolder.setState((state, props) => {
-                        if (this.hand.isHoldingCard() && state.targetStacks[index].stack.indexOf(this.hand.currentCard()) == -1) {
-                            this.removeFromAll()
-                            state.targetStacks[index].stack.push(this.hand.currentCard());
-                            state.targetStacks[index].acceptedCards.pop();
-                        }
 
+    click = (index) => {
+        if (!this.hand.isHoldingCard()) {
+            //@todo allow pickup from target stack (?)
+        } else {
+            var currentAccepted = this.state().targetStacks[index].acceptedCards[this.state().targetStacks[index].acceptedCards.length - 1];
+            if (this.state().targetStacks[index].icon == this.hand.currentCard().props.type.icon && currentAccepted == this.hand.currentCard().props.face) {
+                this.stateHolder.setState((state, props) => {
+                    if (this.hand.isHoldingCard() && state.targetStacks[index].stack.indexOf(this.hand.currentCard()) == -1) {
+                        this.removeFromAll() //@todo remove hack
+                        state.targetStacks[index].stack.push(this.hand.currentCard());
+                        state.targetStacks[index].acceptedCards.pop();
                         return { ...this.unselectCard(state) };
-                    });
-                } else {
-                    this.blink(index);
-                }
+                    } else {
+                        return { ...state };
+                    }
+                });
             } else {
                 this.blink(index);
             }
-        } else {
-            //@todo allow pickup from target stack (?)
         }
-    }
-
-    toggleBlink = (index, blinkFor, cb) => {
-        this.stateHolder.setState((state, props) => {
-            state.targetStacks[index].blinkFor = blinkFor;
-            return { ...state };
-        }, cb);
     }
 
     blink = (index) => {
@@ -42,5 +34,12 @@ export default class TargetStack extends Base {
                 this.toggleBlink(index, 0, () => { });
             }, 100);
         })
+    }
+
+    toggleBlink = (index, blinkFor, cb) => {
+        this.stateHolder.setState((state, props) => {
+            state.targetStacks[index].blinkFor = blinkFor;
+            return { ...state };
+        }, cb);
     }
 }
