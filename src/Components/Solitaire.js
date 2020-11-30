444048444ccb85d61b31f282365a38a13e18b3a6
ckonig <itckoenig@gmail.com>
2020-11-30 20:21:08 +0100

    no more html tables for layout

--- src/Components/Solitaire.js
+++ src/Components/Solitaire.js
@@ -8,7 +8,7 @@ import Hand from './Hand';
 import StockPile from './StockPile';
 import TableauStack from './TableauStack';
 import Waste from './Waste';
-import { targetStackStyle } from '../styles';
+import { styles } from '../styles';
 
 class Solitaire extends Component {
 
@@ -18,67 +18,57 @@ class Solitaire extends Component {
     this.state = this.engine.getInitialState();
   }
 
-  //@todo use CSS instead of HTML table for layout
   render() {
+    const style = styles.solitaire;
+    const Box = (props) => <div style={style.stackbox}>{props.children}</div>
     return (
-      <div>
+      <div style={style.table}>
         <Hand stack={this.state.hand.stack} />
-        <div style={targetStackStyle.tableStyle}>
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <StockPile
-                    clickStockPile={this.engine.stock.clickStockPile}
-                    stockPile={this.state.stockPile} />
-                </td>
-                <td>
-                  <Waste
-                    clickOnWaste={this.engine.stock.clickWaste}
-                    stack={this.state.waste} />
-                </td>
-                <td style={{ width: '100px' }}>&nbsp;</td>
-                {this.state.foundations.map((foundation, index) => (
-                  <td>
-                    <Foundation
-                      stack={foundation.stack}
-                      index={index}
-                      blinkFor={foundation.blinkFor}
-                      icon={foundation.icon}
-                      onFoundationClick={(c) => this.engine.foundation.click(index, c)}
-                    />
-                  </td>
-                ))}
-                {/*@todo move to display component*/}
-                <td>
-                  Points: {this.state.points}
-                  <br />
-                  <Clock started={this.state.started} end={this.state.end} />
-                  <br />
-                  Is Ended: {this.state.isEnded ? "Y" : "N"}
-                </td>
-              </tr>
-              <tr>
-                <td colSpan="7">
-                  <table>
-                    <tbody>
-                      <tr>
-                        {this.state.stacks.map((stack, index) => (
-                          <td>
-                            <TableauStack
-                              stackIndex={index}
-                              stack={stack.stack}
-                              blinkFor={stack.blinkFor}
-                              onClick={(card, source) => this.engine.tableauStack.click(card, index, source)}
-                            /></td>))}
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-                <td>&nbsp;</td>
-              </tr>
-            </tbody>
-          </table>
+        <div style={style.upperContainer}>
+          <Box>
+            <StockPile
+              clickStockPile={this.engine.stock.clickStockPile}
+              stockPile={this.state.stockPile} />
+          </Box>
+          <Box>
+            <Waste
+              clickOnWaste={this.engine.stock.clickWaste}
+              stack={this.state.waste} />
+          </Box>
+          <Box>&nbsp;</Box>
+          <div>
+            {this.state.foundations.map((foundation, index) => (
+              <Box>
+                <Foundation
+                  stack={foundation.stack}
+                  index={index}
+                  blinkFor={foundation.blinkFor}
+                  icon={foundation.icon}
+                  onFoundationClick={(c) => this.engine.foundation.click(index, c)}
+                />
+              </Box>
+            ))}
+          </div>
+          <div style={style.middleContainer}>
+            {this.state.stacks.map((stack, index) => (
+              <Box>
+                <TableauStack
+                  stackIndex={index}
+                  stack={stack.stack}
+                  blinkFor={stack.blinkFor}
+                  onClick={(card, source) => this.engine.tableauStack.click(card, index, source)}
+                />
+              </Box>))}
+          </div>
+          <div style={style.lowerContainer}>
+
+            Points: {this.state.points}
+            <br />
+            <Clock started={this.state.started} end={this.state.end} />
+            <br />
+          Is Ended: {this.state.isEnded ? "Y" : "N"}
+
+          </div>
         </div>
       </div>
     );
--- src/styles.js
+++ src/styles.js
@@ -74,9 +74,26 @@ export const targetStackStyle = {
         ...faceStyle,
         color: 'red'
     },
-    tableStyle: {
-        backgroundColor: 'darkgreen',
-        height: '600px',
-        zIndex: -1,
+
+}
+var baseStyles = {
+    baseContainer: {
+        position: 'absolute',
+        left: '0px',
+        width: '800px'
     },
+}
+export const styles = {
+    solitaire: {
+
+        upperContainer: { ...baseStyles.baseContainer, top: '0px', height: '200px' },
+        middleContainer: { ...baseStyles.baseContainer, top: '200px', height: '400px' },
+        lowerContainer: { ...baseStyles.baseContainer, top: '600px', },
+        table: {
+            backgroundColor: 'darkgreen',
+            height: '600px',
+            zIndex: -1,
+        },
+        stackbox: { width: '110px', float: 'left' },
+    }
 }
