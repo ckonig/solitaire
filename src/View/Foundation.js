5ed27157d868acbc492ebcdc634ea826f5b611cc
ckonig <itckoenig@gmail.com>
2020-12-16 18:57:17 +0100

    tiny cleanup

--- src/View/Foundation.js
+++ src/View/Foundation.js
@@ -17,16 +17,14 @@ export default class Foundation extends BlinkingComponent {
             />
         ));
     render() {
-        const props = this.props;
         const model = this.props.model;
         const stackindex = this.props.stackindex;
-        const settings = this.props.settings;
         return (
             <div key={stackindex}>
                 <StackBase
                     suggested={model.suggestion && !model.stack.length}
                     blink={model.blinkFor}
-                    onClick={() => props.onClick(null, null, stackindex)}
+                    onClick={() => this.props.onClick(null, null, stackindex)}
                     visible={!model.stack.length}
                 >
                     <div className={"align-center foundation-base suit-" + model.icon}>{model.icon}</div>
@@ -39,16 +37,16 @@ export default class Foundation extends BlinkingComponent {
                         shadowOffsetY={-3}
                         blink={model.blinkFor}
                         isSuggested={model.suggestion && model.stack.length - 1 == index}
-                        onClick={(c, p) => props.onClick(c, p, stackindex)}
+                        onClick={(c, p) => this.props.onClick(c, p, stackindex)}
                     />
                 ))}
                 <Hand
-                    settings={settings}
+                    settings={this.props.settings}
                     parent={"foundation-" + stackindex}
                     shadowOffsetX={stackindex * 2}
                     shadowOffsetY={-3}
-                    onClick={(c, p) => props.onClick(model.stack[model.stack.length - 1], p, stackindex)}
-                    model={props.hand}
+                    onClick={(c, p) => this.props.onClick(model.stack[model.stack.length - 1], p, stackindex)}
+                    model={this.props.hand}
                     stack={model.stack}
                 />
             </div>
