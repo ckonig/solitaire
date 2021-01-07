b2d34f01a959e34e21a17d887835d806e6e75eea
ckonig <itckoenig@gmail.com>
2021-01-08 00:38:00 +0100

    fix ID warning

--- src/View/UI/StartScreen/Menu/MenuTree.tsx
+++ src/View/UI/StartScreen/Menu/MenuTree.tsx
@@ -7,12 +7,14 @@ const MenuTree = (props: { children: any[]; keyboardLayout: KeyboardLayout }) =>
     const navigator = new TreeNavigator();
     navigator.rows = [];
     let index = -1;
+    let _i = -1;
     const addItem = (child: any) => {
+        _i++;
         if (!child.props.skip) {
             index++;
             navigator.rows[index] = { x: index, y: 0, ...child.props, buttons: [] };
         }
-        return React.cloneElement(child, { key: index, x: index, y: 0, navigator: navigator });
+        return React.cloneElement(child, { key: _i, x: index, y: 0, navigator: navigator });
     };
     return (
         <>
