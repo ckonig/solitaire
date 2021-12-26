882b91361bca38017e60396abd2a4a47c1a40dae
ckonig <itckoenig@gmail.com>
2021-12-26 01:34:57 +0100

    testing keyboard nav

--- cypress/integration/menu/menu_spec.js
+++ cypress/integration/menu/menu_spec.js
@@ -19,5 +19,28 @@ describe("Menu", () => {
             cy.contains("Support");
             cy.contains("Entropy");
         });
+        describe("keyboard navigation", () => {
+            it.only("Up/down works as expected", () => {
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Single Player");
+                cy.get("body").type("{downarrow}");
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Versus");
+                cy.get("body").type("{downarrow}");
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Options");
+                cy.get("body").type("{downarrow}");
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Allow Cookie");
+                cy.get("body").type("{downarrow}");
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Single Player");
+                cy.get("body").type("{downarrow}");
+                cy.get("body").type("{q}");
+                cy.get("body").type("{downarrow}");
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Player 1");
+                cy.get("body").type("{downarrow}");
+                cy.get("body").type("{downarrow}");
+                cy.get("body").type("{downarrow}");
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Options");
+                cy.get("body").type("{uparrow}");
+                cy.get(".highlight").invoke("attr", "title").should("eq", "Start");
+            });
+        });
     });
 });
