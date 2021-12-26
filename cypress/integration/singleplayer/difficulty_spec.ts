0d2d6e43f4c31fec645439ff2c5c354d0767d808
ckonig <itckoenig@gmail.com>
2021-12-26 22:34:08 +0100

    testing heart icons

--- cypress/integration/singleplayer/difficulty_spec.ts
+++ cypress/integration/singleplayer/difficulty_spec.ts
@@ -8,18 +8,45 @@ describe("SinglePlayer", () => {
             cy.contains("Single Player").click();
             cy.dealFromStock().assertWasteSize(j);
         };
-        describe("single draw pulls one card from stock to waste ", () => {
-            it("no limit", () => validateDraw(0, 1));
-            it("three passes", () => validateDraw(1, 1));
-            it("single pass", () => validateDraw(2, 1));
+        describe("draw behavior", () => {
+            describe("single draw pulls one card from stock to waste ", () => {
+                it("no limit", () => validateDraw(0, 1));
+                it("three passes", () => validateDraw(1, 1));
+                it("single pass", () => validateDraw(2, 1));
+            });
+            describe("triple draw pulls three cards from stock to waste", () => {
+                it("no limit", () => validateDraw(3, 3));
+                it("three passes", () => validateDraw(4, 3));
+                it("single pass", () => validateDraw(5, 3));
+            });
         });
-        describe("triple draw pulls three cards from stock to waste", () => {
-            it("no limit", () => validateDraw(3, 3));
-            it("three passes", () => validateDraw(4, 3));
-            it("single pass", () => validateDraw(5, 3));
+        describe("heart icons", () => {
+            const validateHearts = (i, nrOfHearts) => {
+                cy.withConfig((config) => {
+                    config.quickDeal = true;
+                    config.difficultySettings = 0;
+                }).visit("http://localhost:3000/solitaire");
+                cy.contains("Single Player").click();
+                cy.get(".heart-container").within(() => {
+                    cy.get("svg").should("have.length", 1);
+                });
+            };
+            describe("no limit shows 1 heart", () => {
+                //@todo differentiate single icons
+                it("single draw", () => validateHearts(0, 1));
+                it("triple draw", () => validateHearts(3, 1));
+            });
+            describe("1 pass shows 1 heart", () => {
+                //@todo differentiate single icons
+                it("single draw", () => validateHearts(1, 1));
+                it("triple draw", () => validateHearts(4, 1));
+            });
+            describe("3 pass shows three hearts", () => {
+                it("single draw", () => validateHearts(2, 3));
+                it("triple draw", () => validateHearts(5, 3));
+            });
         });
-    });
 
-    //@todo validate heartContainer content based on difficulty
-    //@todo mock desk to validate recycling efficiently
+        //@todo mock desk to actually validate recycling, but efficiently
+    });
 });
